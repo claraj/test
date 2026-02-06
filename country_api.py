0e66167d218e9d4f81a91516bfb31ec0795ab228
Clara <10088152+claraj@users.noreply.github.com>
2026-02-06 17:37:27 -0600

    use restcountries API

--- country_api.py
+++ country_api.py
@@ -1,28 +1,35 @@
 import requests 
 
 def get_country_name(country_code):
+    """ Returns a tuple of found, country name, error 
+    If country is found, the tuple will be (True, country name, None)
+    If country is not found, the tuple will be (False, None, None)
+    If there is an error connecting to the API. the tuple will be (False, None, error message)"""
+
     try:
         url = create_url(country_code)
         json_response = make_api_request(url)
+        if not json_response:
+             return False, None, None 
         name = get_name_from_response(json_response)
-        return name
-    except:
-        return 'Error, could not get capital city'
-        
+        return True, name, None
+    except Exception as e:
+        return False, None, 'Error connecting to API'
 
 def create_url(country_code):
-    url = f'https://api.worldbank.org/v2/country/{country_code}?format=json'
+    url = f'https://restcountries.com/v3.1/alpha/{country_code}'
     return url
 
 
 def make_api_request(url):
     response = requests.get(url)
+    if response.status_code == 404:
+        return None
     response.raise_for_status()
     json = response.json()  
     return json
 
 
 def get_name_from_response(json_response):
-    # The response is a dictionary with a name key, in a list in a list. 
-    name = json_response[1][0]['name']
+    name = json_response[0]['name']['official']
     return name
--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 This prograam allows a user to look up a country's name from a country code. 
 
-Data is fetched from the World Bank API. 
+Data is fetched from the Rest Countries API, https://restcountries.com/
